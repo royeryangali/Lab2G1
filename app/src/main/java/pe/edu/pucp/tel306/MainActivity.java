143c81d24fcab14a3ac04a21048b0a5bed92f640
Fernando <a20162001@pucp.edu.pe>
2020-09-29 10:53:50 -0500

    fer

--- app/src/main/java/pe/edu/pucp/tel306/MainActivity.java
+++ app/src/main/java/pe/edu/pucp/tel306/MainActivity.java
@@ -3,6 +3,7 @@ package pe.edu.pucp.tel306;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +11,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void mandarParametros(View view){
+
+
     }
 
 
