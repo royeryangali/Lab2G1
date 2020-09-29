c844430c107a294e7d167002f8544ded3c201d05
Fernando <a20162001@pucp.edu.pe>
2020-09-29 11:13:35 -0500

    fer

--- app/src/main/java/pe/edu/pucp/tel306/MainActivity.java
+++ app/src/main/java/pe/edu/pucp/tel306/MainActivity.java
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
+
+import pe.edu.pucp.tel306.Entity.Usuario;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,6 +17,22 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void mandarParametros(View view){
+        TextView textViewNombre = findViewById(R.id.nomcam);
+        String nomcam = textViewNombre.getText().toString();
+
+        TextView textViewApellido = findViewById(R.id.apecam);
+        String apecam = textViewApellido.getText().toString();
+
+        TextView textViewEspecialidad = findViewById(R.id.espcam);
+        String espcam = textViewEspecialidad.getText().toString();
+
+        TextView textViewDNI = findViewById(R.id.dnicam);
+        String dnicam = textViewDNI.getText().toString();
+
+        TextView textViewCam = findViewById(R.id.clavecam);
+        String clavecam = textViewCam.getText().toString();
+
+        Usuario usu = new Usuario();
 
 
     }
