026f6f706ede08bda46a5fab8a63792022d49120
Royer <r.yangali@pucp.pe>
2020-09-29 10:39:19 -0500

    arreglando lo de julio

---
+++ app/src/main/java/pe/edu/pucp/tel306/tareas_pendientes.java
@@ -0,0 +1,14 @@
+package pe.edu.pucp.tel306;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class tareas_pendientes extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tareas_pendientes);
+    }
+}
