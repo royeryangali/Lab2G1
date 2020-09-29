c2679b2ac2bb52c0c2aeab1b38fb774c928623d1
Julio Carrion <a20161448@pucp.edu.pe>
2020-09-29 13:35:24 -0500

    80%

--- app/src/main/java/pe/edu/pucp/tel306/tareas_pendientes.java
+++ app/src/main/java/pe/edu/pucp/tel306/tareas_pendientes.java
@@ -3,13 +3,16 @@ package pe.edu.pucp.tel306;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.text.Layout;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -18,7 +21,7 @@ import pe.edu.pucp.tel306.Entity.Usuario;
 public class tareas_pendientes extends AppCompatActivity {
 
     ArrayList<Integer> checkedlist = new ArrayList<Integer>();
-
+    int clickcount=0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,6 +37,16 @@ public class tareas_pendientes extends AppCompatActivity {
         TextView noHayTAreas = findViewById(R.id.textView4);
         LinearLayout layout = findViewById(R.id.linearLayoutCheck);
         ImageButton imageButton = findViewById(R.id.imageButton2);
+        imageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                clickcount=clickcount+1;
+                if(clickcount==5) {
+                    Toast.makeText(getApplicationContext(),"Has desbloqueado la opción oscura", Toast.LENGTH_LONG).show();
+                    clickcount = 0;
+                }
+            }
+        });
 
         if(usuario.getEspecialidad().equalsIgnoreCase("Telecomunicaciones")){
             imageButton.setImageResource(R.drawable.fibra);
@@ -65,6 +78,15 @@ public class tareas_pendientes extends AppCompatActivity {
                 int request = 1;
                 intent.putExtra("usu2", usuario);
                 startActivityForResult(intent,request);
+            }
+        });
+
+        Button volverAlFormulario = findViewById(R.id.volverFormularioButton);
+        volverAlFormulario.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(tareas_pendientes.this,agregar_tarea.class);
+                startActivity(intent);
             }
         });
 
